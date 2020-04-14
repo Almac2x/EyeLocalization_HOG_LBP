28093371205c4ae892d31be444d236427b31265b
Almac2x <blando_macmac@yahoo.com>
2020-04-14 17:50:32 +0800

    Eye_Detection.py Confidence Level

--- Eye_Detection.py
+++ Eye_Detection.py
@@ -1,9 +1,10 @@
-#Website https://www.pyimagesearch.com/2015/03/23/sliding-windows-for-object-detection-with-python-and-opencv/
+# Website https://www.pyimagesearch.com/2015/03/23/sliding-windows-for-object-detection-with-python-and-opencv/
 
-import cv2,pickle,argparse,time
+import cv2, pickle, argparse, time
 from Feature_Descriptors.LBP import LBP
 from Feature_Descriptors.localbinarypatterns import LocalBinaryPatterns
 from pyimagesearch.helpers import pyramid, sliding_window
+
 
 def getEyes(image):
     # Starts of Eye Detection
@@ -18,13 +19,11 @@ def getEyes(image):
     # initialize the local binary patterns descriptor along with
 
     # load the model from disk
-    Model_Path = "Eye_Detection_Model/LBP_Oversize_Dataset_0.8738 _KF#3.sav"
+    Model_Path = "Eye_Detection_Model/LBP_Oversize_Datasets_0.8676 _KF#3.sav"
     loaded_model = pickle.load(open(Model_Path, 'rb'))
 
-    #Converts to LocalBinaryPatter
+    # Converts to LocalBinaryPatter
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-
 
     # loop over the image pyramid
     for resized in pyramid(image, scale=1.5):
@@ -36,25 +35,25 @@ def getEyes(image):
 
             clone = resized.copy()
 
-
             cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
             cv2.imshow("Window", clone)
 
             crop_img = gray[y:y + winH, x:x + winW]
-            crop_img = cv2.resize(crop_img,(64,64),interpolation = cv2.INTER_AREA)
+            crop_img = cv2.resize(crop_img, (64, 64), interpolation=cv2.INTER_AREA)
 
-
-            hist = desc.describe(crop_img,"Frame")
+            hist = desc.describe(crop_img, "Frame")
             # Loads Prediction Model
             prediction = loaded_model.predict(hist.reshape(1, -1))
 
+            confidence_level = loaded_model.decision_function(hist.reshape(1, -1))
+
             print(prediction[0])
+            print("Confidence Level: {}".format(loaded_model.decision_function(hist.reshape(1, -1))))
 
             # display the image and the prediction
-            XY = (x, y + winH)
-            cv2.putText(image, prediction[0], XY, cv2.FONT_HERSHEY_SIMPLEX,
-                        1.0, (0, 0, 255), 3)
-
+            # XY = (x, y + winH)
+            # cv2.putText(image, prediction[0], XY, cv2.FONT_HERSHEY_SIMPLEX,
+            #           1.0, (0, 0, 255), 3)
 
             cv2.imshow("cropped", crop_img)
             cv2.waitKey(0)
@@ -65,9 +64,3 @@ def getEyes(image):
             # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
             # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
             # WINDOW
-
-
-
-
-
-
