c7bdd6cc4f1785654e5e273f2c88dd582cdce329
nathpili <nathpili.e@gmail.com>
2020-04-26 00:24:27 +0800

    save eyes 2v

--- Test_Create_Dataset/Cropped_Face_Parts.py
+++ Test_Create_Dataset/Cropped_Face_Parts.py
@@ -1,7 +1,5 @@
 from sklearn.model_selection import KFold
 
-from Descriptors.HOG import HOG
-from Descriptors.localbinarypatterns import LocalBinaryPatterns
 from sklearn.svm import LinearSVC
 import argparse
 import cv2
@@ -15,16 +13,19 @@ from imutils import paths
 from pyimagesearch.helpers import pyramid, sliding_window
 
 # load the image and define the window width and height
-(winW, winH) = (64,64)
+(winW, winH) = (64, 64)
 
-#Change Dataset
-dataset_path = r"D:\Documents\Chrome Downloads\Thesis Download\Datasets\CroppedYale_ConvertedPNG"
-image_path_save = r"D:\Documents\Chrome Downloads\Thesis Download\Datasets\Cropped_64x64\.."
+# Change Dataset
+dataset_path = r"..\dataset\trisha"
+image_path_save = r"C:\Users\Pili\PycharmProjects\EyeLocalization_HOG_LBP\Test_Create_Dataset"
+image_path_save_eyes = r"C:\Users\Pili\PycharmProjects\EyeLocalization_HOG_LBP\Test_Create_DatasetEyes"
+
+
+# os.mkdir(image_path_save_eyes)
 
 def scan_image(image, name):
     # loop over the image pyramid
-    count=1
-    os.mkdir(image_path_save + name)
+    count = 1
     for (x, y, window) in sliding_window(image, stepSize=64, windowSize=(winW, winH)):
         # if the window does not meet our desired window size, ignore it
         if window.shape[0] != winH or window.shape[1] != winW:
@@ -32,13 +33,14 @@ def scan_image(image, name):
         crop_img = image[y:y + winH, x:x + winW]
 
         # Writes the cropped to disk
+        if count == 1 or count == 2:
+            cv2.imwrite('%s/%s-%s.png' % (image_path_save_eyes, name, count), crop_img)
+            print("image save")
+        else:
+            cv2.imwrite('%s/%s-%s.png' % (image_path_save, name, count), crop_img)
+            print("image save")
 
-        new_image_path_save = image_path_save++name
-
-        cv2.imwrite('%s/%s-%s.png' % (new_image_path_save, str(name+count), count), crop_img)
-        print("image save")
-
-        #Shows How Sliding windows works
+        # Shows How Sliding windows works
         count += 1
         clone = image.copy()
         cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 255), 2)
@@ -49,8 +51,6 @@ def scan_image(image, name):
 data = []
 labels = []
 
-
-
 # loop over the training images
 
 for imagePath in paths.list_images(dataset_path):
@@ -59,11 +59,11 @@ for imagePath in paths.list_images(dataset_path):
     image = cv2.imread(imagePath)
     print("Image Path: {} \n Image Height: {} \n Image Width: {} ".format(imagePath.split(os.path.sep)[-1],
                                                                           image.shape[0], image.shape[1]))
-    resize = cv2.resize(image, (192,192), interpolation=cv2.INTER_AREA)
+    resize = cv2.resize(image, (192, 192), interpolation=cv2.INTER_AREA)
     gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
     cv2.imshow(str(imagePath), resize)
 
-    scan_image(gray, imagePath.split(os.path.sep)[-1])
+    scan_image(gray, os.path.splitext(imagePath.split(os.path.sep)[-1])[0])
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
