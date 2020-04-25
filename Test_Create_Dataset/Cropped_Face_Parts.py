d88885d713579806bbc0077dc1405074c6074941
Almac2x <alejandro_blando@yahoo.com>
2020-04-26 00:00:27 +0800

    Fix Pili

--- Test_Create_Dataset/Cropped_Face_Parts.py
+++ Test_Create_Dataset/Cropped_Face_Parts.py
@@ -17,19 +17,28 @@ from pyimagesearch.helpers import pyramid, sliding_window
 # load the image and define the window width and height
 (winW, winH) = (64,64)
 
-image_path = r"C:\Users\Pili\PycharmProjects\EyeLocalization_HOG_LBP\Test_Create_Dataset"
+#Change Dataset
+dataset_path = r"D:\Documents\Chrome Downloads\Thesis Download\Datasets\CroppedYale_ConvertedPNG"
+image_path_save = r"D:\Documents\Chrome Downloads\Thesis Download\Datasets\Cropped_64x64\.."
 
 def scan_image(image, name):
     # loop over the image pyramid
     count=1
+    os.mkdir(image_path_save + name)
     for (x, y, window) in sliding_window(image, stepSize=63, windowSize=(winW, winH)):
         # if the window does not meet our desired window size, ignore it
         if window.shape[0] != winH or window.shape[1] != winW:
             continue
         crop_img = image[y:y + winH, x:x + winW]
+
         # Writes the cropped to disk
-        cv2.imwrite('%s/%s-%s.png' % (image_path, name, count), crop_img)
+
+        new_image_path_save = image_path_save++name
+
+        cv2.imwrite('%s/%s-%s.png' % (new_image_path_save, str(name+count), count), crop_img)
         print("image save")
+
+        #Shows How Sliding windows works
         count += 1
         clone = image.copy()
         cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 255), 2)
@@ -40,13 +49,13 @@ def scan_image(image, name):
 data = []
 labels = []
 
-dataset_path = r"../dataset/trisha"
+
 
 # loop over the training images
 
 for imagePath in paths.list_images(dataset_path):
     # load the image, convert it to grayscale, and describe it
-    print("JEff")
+
     image = cv2.imread(imagePath)
     print("Image Path: {} \n Image Height: {} \n Image Width: {} ".format(imagePath.split(os.path.sep)[-1],
                                                                           image.shape[0], image.shape[1]))
